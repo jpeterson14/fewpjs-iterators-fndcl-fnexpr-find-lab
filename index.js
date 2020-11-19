4d27ace5ccded296248006cd4ee5ae7b8fac62da
jpeterson14 <jpeterson0514@gmail.com>
2020-11-18 21:38:39 -0600

    commit tree 6e94e492e5e4a05555d4ae379b3b5624bd127982

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array){
+  let stats = array.find(stat => stat.result === "W")
+
+if (stats){
+  return stats.year
+} else {
+  return stats
+}
+}
